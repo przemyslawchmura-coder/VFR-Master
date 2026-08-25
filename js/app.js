4bc07f27aba58dad8331771bf1b8bb492ba62634
przemyslawchmura-coder <przemyslaw.chmura@o2.pl>
2026-08-25 21:25:01 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+const VFRApp = {
 
+  init() {
+    console.log("VFR Master uruchomiony");
+
+    if (window.VFR800_2002) {
+      console.log(
+        "Załadowano bazę:",
+        VFR800_2002.manufacturer,
+        VFR800_2002.model,
+        VFR800_2002.year
+      );
+    }
+
+    if (window.MotorcycleDatabase) {
+      console.log(
+        "Liczba motocykli w garażu:",
+        MotorcycleDatabase.getAll().length
+      );
+    }
+  },
+
+  getBikeData() {
+    return window.VFR800_2002;
+  },
+
+  getGarage() {
+    return window.MotorcycleDatabase.getAll();
+  }
+};
+
+window.VFRApp = VFRApp;
+
+document.addEventListener("DOMContentLoaded", () => {
+  VFRApp.init();
+});
