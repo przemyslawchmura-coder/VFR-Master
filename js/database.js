2f8c423db1edaf9be08751e7fa9005f8a6605a52
przemyslawchmura-coder <przemyslaw.chmura@o2.pl>
2026-08-25 21:17:12 +0200

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,1 +1,39 @@
+const MotorcycleDatabase = {
 
+  motorcycles: [],
+
+  add(motorcycle) {
+    this.motorcycles.push(motorcycle);
+    this.save();
+  },
+
+  remove(index) {
+    this.motorcycles.splice(index, 1);
+    this.save();
+  },
+
+  getAll() {
+    return this.motorcycles;
+  },
+
+  save() {
+    localStorage.setItem(
+      "vfrMasterMotorcycles",
+      JSON.stringify(this.motorcycles)
+    );
+  },
+
+  load() {
+    const saved = localStorage.getItem(
+      "vfrMasterMotorcycles"
+    );
+
+    if (saved) {
+      this.motorcycles = JSON.parse(saved);
+    }
+  }
+};
+
+MotorcycleDatabase.load();
+
+window.MotorcycleDatabase = MotorcycleDatabase;
